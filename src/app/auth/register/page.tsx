63ec8312517b75a0ed8d550bc93392d9897c134a
Fritz Schroeder <Schrooge@gmail.com>
2026-01-14 19:43:01 +0000

    change this place holder picture with one in the public library called P

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -13,7 +13,7 @@ export default function RegisterPage() {
                     className="absolute inset-0 bg-cover"
                     style={{
                         backgroundImage:
-                        'url(/DRedger.png)',
+                        'url(/Pump.jpg)',
                     }}
                 />
                  <div className="relative z-20 flex items-center text-lg font-medium">
