645e977bb5ad19c9e913a7fd434a107e5ad3cd05
Fritz Schroeder <Schrooge@gmail.com>
2026-01-20 08:56:11 +0000

    ok upload the 2 record undr iron injection Fume Hood:

--- src/lib/seed-data.ts
+++ src/lib/seed-data.ts
@@ -9,5 +9,30 @@ const nextMaintenanceDate = format(new Date(new Date().setMonth(new Date().getMo
 // This file is ready for future data seeding.
 // To add new equipment, populate this array following the example structure.
 export const equipmentToSeed: Array<Omit<Equipment, 'id' | 'vsdId' | 'status' > & { model: string; serialNumber: string, installationDate: string, driveType?: 'VSD' | 'Soft Starter', manufacturer?: string }> = [
-  
+  {
+    name: 'Injection station hood 1',
+    location: 'Iron Injection - Fumehood',
+    plant: 'Smelter',
+    division: 'Iron Injection',
+    lastMaintenance: todayFormatted,
+    nextMaintenance: nextMaintenanceDate,
+    model: 'UNISP',
+    serialNumber: 'fumehood-1-sn',
+    installationDate: todayFormatted,
+    driveType: 'VSD',
+    manufacturer: 'Control Techniques',
+  },
+  {
+    name: 'Injection station hood 2',
+    location: 'Iron Injection - Fumehood',
+    plant: 'Smelter',
+    division: 'Iron Injection',
+    lastMaintenance: todayFormatted,
+    nextMaintenance: nextMaintenanceDate,
+    model: 'UNISP',
+    serialNumber: 'fumehood-2-sn',
+    installationDate: todayFormatted,
+    driveType: 'VSD',
+    manufacturer: 'Control Techniques',
+  },
 ];
